577b51b85facebc86fb683eeefeb616a93d4f467
holasebas <danielpalma2003@hotmail.com>
2020-11-02 12:19:30 -0500

    set type server to the param server

--- src/websocket.ts
+++ src/websocket.ts
@@ -1,5 +1,6 @@
-import socketIO from "socket.io"
-export default function initsocket(server: any) {
+import socketIO from "socket.io";
+import { Server } from "http";
+export default function initsocket(server: Server) {
     const io = socketIO(server)
     io.on("connection", (socket) => {
         socket.emit("server:test");
