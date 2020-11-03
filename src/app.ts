16cad683ed194e580531568fad78c5b15fbb21a7
Daniel Palma Garcia <danielpalma2003@hotmail.com>
2020-11-03 10:06:42 -0500

    added format to the code

--- src/app.ts
+++ src/app.ts
@@ -37,15 +37,16 @@ export default class App {
         this.app.use(routerMain);
         this.app.use(routerUsers);
         this.app.use(express.static(join(__dirname, "public")));
-        this.app.use((req: Request, res: Response) => {
+        this.app.use((_req: Request, res: Response) => {
             return res.status(404).send("this page doesn't exist");
         });
-        this.app.use((err: ErrorRequestHandler, req: Request, res: Response, next: NextFunction) => {
+        this.app.use((err: ErrorRequestHandler, _req: Request, res: Response, next: NextFunction) => {
             console.error(err);
             res.status(500).send('Oh sh*t something broke');
-        })
+            next();
+        });
     };
-    public listen(callback: Function = () => { console.log(`Server on port ${this.app.get("port")}`) }): Server {
+    public listen(callback: Function = () => console.log(`Server on port ${this.app.get("port")}`)): Server {
         const server = this.app.listen(this.app.get("port"), callback());
         initsoket(server);
         return server;
